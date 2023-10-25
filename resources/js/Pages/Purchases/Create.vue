28b6d08fe0f1c0b43b5e9a842d2d785cc31206b3
TadunaKitamura <taduna.kitamura0324@gmail.com>
2023-10-25 10:18:36 +0900

    micromodal.jsによる顧客検索機能の実装

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -11,7 +11,7 @@ import MicroModal from '@/Components/MicroModal.vue'
 
 const props = defineProps({
   // errors: Object,
-  'customers': Array,
+  // 'customers': Array,
   'items': Array
 })
 
@@ -58,9 +58,9 @@ const storePurchase = () => {
 
 const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
 
-// const setCustomerId = id => {
-//   form.customer_id = id
-// }
+const setCustomerId = id => {
+  form.customer_id = id
+}
 
 </script>
 
@@ -93,14 +93,8 @@ const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
         <div class="p-2 w-full">
           <div class="relative">
-            <MicroModal/>
-  <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
-  <select name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-    <option v-for="customer in
-customers" :value="customer.id" :key="customer.id">
-{{ customer.id }} : {{ customer.name }}
-</option>
-  </select>
+            <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
+            <MicroModal @update:customerId="setCustomerId" />
           </div>
         </div>
 
@@ -132,7 +126,7 @@ customers" :value="customer.id" :key="customer.id">
     </div>
  
         <div class="p-2 w-full">
-          <div class="relative">
+          <div class="">
             <label for="price" class="leading-7 text-sm text-gray-600">合計金額</label><br>
             <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
               {{ totalPrice }} 円
